c7b95cc2d94d22e0608f7f5916da59151e8eb16f
kacperwalter <52107523+kacperwalter@users.noreply.github.com>
2024-04-23 20:49:39 +0200

    Add markup to about me page

--- app/common/components/sections/about-me/about-me.tsx
+++ app/common/components/sections/about-me/about-me.tsx
@@ -1,32 +1,52 @@
 import Image from "next/image"
-import { PortableText } from "next-sanity"
+import Wrapper from "../../partials/wrapper/wrapper"
+import Heading from "../../partials/heading/heading"
 import "./about-me.scss"
 import type { AboutMeProps } from "./about-me.types"
+import { PortableText } from "next-sanity"
 
 const AboutMe = ({ content }: AboutMeProps) => {
   return (
     <section className="about-me">
-      <Image
-        src={content.aboutMeTopImage}
-        alt="Top image"
-        width={400} // Adjust the width as needed
-        height={250} // Adjust the height as needed
-      />
-      <h2>{content.aboutMeHeading}</h2>
-      <p>{content.aboutMeCaption}</p>
-      <Image
-        src={content.aboutMeFirstContentImage}
-        alt="First content image"
-        width={400} // Adjust the width as needed
-        height={250} // Adjust the height as needed
-      />
-      <PortableText value={content.aboutMeContent} />
-      <Image
-        src={content.aboutMeSecondContentImage}
-        alt="Second content image"
-        width={400} // Adjust the width as needed
-        height={250} // Adjust the height as needed
-      />
+      <Wrapper>
+        <div className="about-me__top">
+          <Image
+            src={content.aboutMeTopImage}
+            alt="Top image"
+            width={200} 
+            height={200}
+          />
+
+          <div className="header">
+            <Heading
+              type="h1"
+              text={content.aboutMeHeading}
+            />
+
+            {/* @ts-ignore */}
+            <p>{content.aboutMeCaption}</p>
+          </div>
+        </div>
+
+        <div className="about-me__bottom">
+          <Image 
+            src={content.aboutMeFirstContentImage}
+            alt="Top image"
+            width={200} 
+            height={200}
+          />
+
+          {/* @ts-ignore */}
+          <p>{content.aboutMeContent}</p>
+
+          <Image 
+            src={content.aboutMeSecondContentImage}
+            alt="Top image"
+            width={200} 
+            height={200}
+          />
+        </div>
+      </Wrapper>
     </section>
   )
 }
--- app/common/components/sections/about-me/about-me.types.ts
+++ app/common/components/sections/about-me/about-me.types.ts
@@ -3,7 +3,7 @@ import { PortableTextBlock } from 'next-sanity'
 export interface AboutMeContent {
   aboutMeTopImage: string
   aboutMeHeading: string
-  aboutMeCaption: string
+  aboutMeCaption: PortableTextBlock[]
   aboutMeFirstContentImage: string
   aboutMeContent: PortableTextBlock | PortableTextBlock[]
   aboutMeSecondContentImage: string
