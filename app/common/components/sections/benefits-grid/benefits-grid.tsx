5e78f8a7947d886603b233d4f22dece700fbd320
kacperwalter <52107523+kacperwalter@users.noreply.github.com>
2024-04-24 22:04:54 +0200

    Add benefits grid component and icons

---
+++ app/common/components/sections/benefits-grid/benefits-grid.tsx
@@ -0,0 +1,21 @@
+import Heading from "../../partials/heading/heading"
+import Wrapper from "../../partials/wrapper/wrapper"
+
+const BenefitsGrid = () => {
+  return (
+    <section className="benefits-grid">
+      <Wrapper>
+        <Heading
+          type="h1"
+          text="Z moim wsparciem:"
+        />
+
+        <div className="benefits-grid__list">
+
+        </div>
+      </Wrapper>
+    </section>
+  )
+}
+
+export default BenefitsGrid
--- app/modules/Homepage/Homepage.tsx
+++ app/modules/Homepage/Homepage.tsx
@@ -2,6 +2,7 @@ import { HomepageProps } from '@/app/types'
 import Hero from '@/app/common/components/sections/hero/hero'
 import AboutMe from '@/app/common/components/sections/about-me/about-me'
 import Qualifications from '@/app/common/components/sections/qualifications/qualifications'
+import BenefitsGrid from '@/app/common/components/sections/benefits-grid/benefits-grid'
 
 const Homepage = ({ content }: HomepageProps) => {
   const homepageData = content[0] || {}
@@ -42,6 +43,7 @@ const Homepage = ({ content }: HomepageProps) => {
       <Hero content={homepageHeroData}/>
       <AboutMe content={homepageAboutMeData} />
       <Qualifications />
+      <BenefitsGrid />
     </main>
   )
 }
