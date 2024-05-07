e2484c19fce06ee706479b7458330e174d508fa4
kacperwalter <52107523+kacperwalter@users.noreply.github.com>
2024-05-07 23:01:52 +0200

    Add text and image mobile layout

--- app/modules/Homepage/Homepage.tsx
+++ app/modules/Homepage/Homepage.tsx
@@ -2,6 +2,7 @@ import { HomepageProps } from '@/app/types'
 import Hero from '@/app/common/components/sections/hero/hero'
 import AboutMe from '@/app/common/components/sections/about-me/about-me'
 import Qualifications from '@/app/common/components/sections/qualifications/qualifications'
+import TextAndImage from '@/app/common/components/sections/text-and-image/text-and-image'
 import BenefitsGrid from '@/app/common/components/sections/benefits-grid/benefits-grid'
 import RichNumberedList from '@/app/common/components/sections/rich-numbered-list/rich-numbered-list'
 import Tiles from '@/app/common/components/sections/tiles/tiles'
@@ -48,6 +49,7 @@ const Homepage = ({ content }: HomepageProps) => {
       {/* @ts-ignore */}
       <AboutMe content={homepageAboutMeData} />
       <Qualifications />
+      <TextAndImage />
       <BenefitsGrid />
       <RichNumberedList />
       <Tiles />
