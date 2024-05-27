df905f8656ed0367fbbfc6e03cf4e0dbf98442b3
kacperwalter <kacwal@poczta.fm>
2024-05-27 21:40:43 +0200

    Change title and description

--- app/page.tsx
+++ app/page.tsx
@@ -11,14 +11,14 @@ import "@/app/common/styles/spacings.scss"
 import "@/app/common/components/partials/header/header.scss" // just temporary
 
 export const metadata: Metadata = {
-  title: "Punkt Mruczenia - Natalia Szajowska-Tomczak kocia behawiorystka",
-  description: "Jestem Natalia i jestem kocią behawiorystką. Pomogę Ci rozwiązać problem behawioralny u Twojego kota.",
+  title: "Punkt Mruczenia | Natalia Szajowska-Tomczak | Kocia behawiorystka",
+  description: "Problem behawioralny u kota? Skonsultuj się z dyplomowanym psychologiem zwierząt. Wizyty w Poznaniu i okolicach oraz online.",
   alternates: {
     canonical: "https://www.punktmruczenia.pl",
   },
   openGraph: {
-    title: "Punkt Mruczenia - Natalia Szajowska-Tomczak kocia behawiorystka",
-    description: "Jestem Natalia i jestem kocią behawiorystką. Pomogę Ci rozwiązać problem behawioralny u Twojego kota.",
+    title: "Punkt Mruczenia | Natalia Szajowska-Tomczak | Kocia behawiorystka",
+    description: "Problem behawioralny u kota? Skonsultuj się z dyplomowanym psychologiem zwierząt. Wizyty w Poznaniu i okolicach oraz online.",
     url: "puntmruczenia.pl",
     siteName: "Punkt Mruczenia",
     images: [
