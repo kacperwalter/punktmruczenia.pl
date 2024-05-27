8f0dade273e64c3b2585616f3c952f12e366c399
kacperwalter <kacwal@poczta.fm>
2024-05-27 19:11:10 +0200

    Add canonical tag

--- app/page.tsx
+++ app/page.tsx
@@ -13,6 +13,9 @@ import "@/app/common/components/partials/header/header.scss" // just temporary
 export const metadata: Metadata = {
   title: "Punkt Mruczenia - Natalia Szajowska-Tomczak kocia behawiorystka",
   description: "Jestem Natalia i jestem kocią behawiorystką. Pomogę Ci rozwiązać problem behawioralny u Twojego kota.",
+  alternates: {
+    canonical: "https://www.punktmruczenia.pl",
+  },
   openGraph: {
     title: "Punkt Mruczenia - Natalia Szajowska-Tomczak kocia behawiorystka",
     description: "Jestem Natalia i jestem kocią behawiorystką. Pomogę Ci rozwiązać problem behawioralny u Twojego kota.",
