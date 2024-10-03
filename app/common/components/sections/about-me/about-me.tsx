f4d7df85942ad535bdd3e62d51aca61b32d5489f
kacperwalter <kacwal@poczta.fm>
2024-10-03 15:26:59 +0200

    O mnie fix

--- app/common/components/sections/about-me/about-me.tsx
+++ app/common/components/sections/about-me/about-me.tsx
@@ -1,3 +1,5 @@
+// about-me.tsx
+
 import Image from "next/image"
 import Wrapper from "../../partials/wrapper/wrapper"
 import Heading from "../../partials/heading/heading"
@@ -10,8 +12,16 @@ import kreskaMiddle from "@/public/images/kreski/about-me-middle.svg"
 import kreskaBottom1 from "@/public/images/kreski/about-me-bottom-1.svg"
 import kreskaBottom2 from "@/public/images/kreski/about-me-bottom-2.svg"
 
-// TODO CMS - content from CMS
 const AboutMe = ({ content }: AboutMeProps) => {
+  const {
+    aboutMeTopImage,
+    aboutMeHeading,
+    aboutMeCaption,
+    aboutMeFirstContentImage,
+    aboutMeContent,
+    aboutMeSecondContentImage,
+  } = content
+
   return (
     <section className="about-me" id="about-me">
       <Image
@@ -33,26 +43,16 @@ const AboutMe = ({ content }: AboutMeProps) => {
       <Wrapper>
         <div className="about-me__top">
           <Image
-            src={content.aboutMeTopImage}
+            src={aboutMeTopImage}
             alt="Top image"
-            width={1000} 
+            width={1000}
             height={1000}
             className="about-me__image"
           />
 
           <div className="header">
-            <Heading
-              type="h2"
-              text={content.aboutMeHeading}
-            />
-
-            <p>
-              Punkt Mruczenia powstał z miłości do zwierząt i przekonania, że bez kotów nic nie byłoby takie samo. W mojej pracy kieruję się empatią, stawiam na pozytywne wzmocnienia, korzystam z fachowej wiedzy i literatury ekspertów z zakresu zoopsychologii oraz kociego behawioru. 
-                <br /><br />
-                <strong>
-                Jestem absolwentką studiów podyplomowych na kierunku psychologia zwierząt na Uniwersytecie SWPS w Poznaniu. Ukończyłam zaawansowany kurs dla behawiorystów kotów prowadzony przez lek. wet. Martynę Woszczyło. Biorę udział w szkoleniach i kursach, które pozwalają lepiej zrozumieć złożoność zachowania kotów.
-                </strong>
-            </p>
+            <Heading type="h2" text={aboutMeHeading} />
+            <p dangerouslySetInnerHTML={{ __html: aboutMeCaption }} /> {/* Render caption as HTML */}
           </div>
         </div>
 
@@ -65,31 +65,25 @@ const AboutMe = ({ content }: AboutMeProps) => {
         />
 
         <div className="about-me__bottom">
-          <Image 
-            src={content.aboutMeFirstContentImage}
-            alt="Top image"
-            width={1000} 
+          <Image
+            src={aboutMeFirstContentImage}
+            alt="First content image"
+            width={1000}
             height={1000}
             className="about-me__image about-me__image--centered"
           />
 
-          <p>
-            Kocham wszystkie zwierzęta, ale to właśnie kotom oddałam największy kawałek mojego futrzastego serca. Przez moje ręce przewinęło się sporo bezdomnych kotów i psów. Nie wszystkim udało się pomóc na czas, ale każdy z nich odcisnął swoją łapkę na ścieżce, która prowadzi tutaj, do Punktu Mruczenia.
+          <p dangerouslySetInnerHTML={{ __html: aboutMeContent }} /> {/* Render content as HTML */}
 
-            <br /><br />
-
-            Na co dzień jestem kocią mamą Uli, adoptowanej, niesłyszącej trikolorki ze zwyrodnieniem kręgosłupa szyjnego. To Ula dała mi najważniejszą lekcję życia ze zwierzakiem: nie wychodź z oczekiwaniami, daj przestrzeń, wesprzyj w zapewnieniu komfortu, a być może wydarzy się magia.
-          </p>
-
-          <Image 
-            src={content.aboutMeSecondContentImage}
-            alt="Top image"
-            width={1000} 
+          <Image
+            src={aboutMeSecondContentImage}
+            alt="Second content image"
+            width={1000}
             height={1000}
             className="about-me__image about-me__image--centered about-me__image--second"
           />
         </div>
-      </Wrapper> 
+      </Wrapper>
 
       <Image
         src={kreskaBottom1}
